4f99514c36a5d44620cc356f6375fc8a76632204
javier <javiergda@gmail.com>
2021-12-23 17:38:42 +0100

    componente header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import bird from '../../icons/bird.png'
+import butterfly from '../../icons/butterfly.png'
+import deer from '../../icons/deer.png'
+import rabbit from '../../icons/rabbit.png'
+import menu from '../../icons/menu.png'
+
+
 
 export const Header = () => {
     return (
-        <div>
-            <h2>Soy el header</h2>
-        </div>
+
+
+        <header className="header__main">
+            <nav className="nav_small">
+                <ul>
+                    <li>
+                        <img src={bird} height="20px" alt='bird' />
+                        <h3><span>Escuela infantil Bambi</span></h3>
+                        <img src={menu} height="20px" alt='' />
+                    </li>
+                    <li>
+                        <img src={bird} height="20px" alt='bird' />
+                        <span>Inicio</span>
+                    </li>
+                    <li>
+                        <img src={butterfly} height="20px" alt='bird' />
+                        <span>Alumno</span>
+                    </li>
+                    <li>
+                        <img src={deer} height="20px" alt='bird' />
+                        <span>Profesor</span>
+                    </li>
+                    <li>
+                        <img src={rabbit} height="20px" alt='bird' />
+                        <span>Contacto</span>
+                    </li>
+                    <li className=''>
+                        <span >Login</span>
+                    </li>
+                </ul>
+            </nav>
+
+            <nav className='nav_medium'>
+                <ul>
+                    <li>
+                        <h3><span>Escuela infantil Bambi</span></h3>
+                    </li>
+                    <li>
+                        <span>Inicio</span>
+                    </li>
+                    <li>
+                        <span>Alumno</span>
+                    </li>
+                    <li>
+                        <span>Profesor</span>
+                    </li>
+                    <li>
+                        <span>Contacto</span>
+                    </li>
+                    <li className=''>
+                        <span >Login</span>
+                    </li>
+                </ul>
+
+
+
+            </nav>
+
+        </header>
+
+
+
+
     )
 }
