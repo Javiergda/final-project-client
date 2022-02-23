a3462404f89b3bc74ed3347907563157f0a911dc
javier <javiergda@gmail.com>
2022-02-23 21:43:33 +0100

    modificacion hook useFetch

--- src/components/Hooks/useFetch.js
+++ src/components/Hooks/useFetch.js
@@ -1,55 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { URL_CRUD } from '../../settings';
 
-export const useFetch = (url, options) => {
+export const useFetch = ({ endPoint, options, body }) => {
 
+    const [data, setData] = useState([]);
 
-    // const options = {
-    //     method: "POST",
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(queryData)//datos desdes fuera queryData
-    // }
+    useEffect(() => {
+        console.log('hook useFetch cargado');
 
-    console.log('hook useFetch cargado');
+        fetch(`${URL_CRUD}/${endPoint}`, options)
+            .then(response => response.json())
+            .then(response => {
+                setData(response)
+            })
+    }, [endPoint])
 
-    fetch(url, options)
-
-        .then(response => response.json())
-        .then(data => {
-            if (data.ok) {
-                return data;
-            } else {
-                console.log(data);
-                return false;
-            }
-        });
-
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const resp = await fetch(`${URL_CRUD}/${endPoint}`, {
-    //                 method: queryMethod,
-    //                 body: JSON.stringify(queryData),
-    //                 headers: {
-    //                     'Content-Type': 'application/json'
-    //                 }
-    //             });
-    //             const { data } = await resp.json();
-    //             return data;
-    //         } catch (error) {
-    //             console.log("error", error);
-    //         }
-    //     };
-    //     fetchData();
-    // }, []);
-
+    return data;
 };
-
-// Llamar hook
-// queryMethod='GET','POST','PUT','DELETE'
-// endPoint='/create','/read','/update','/delete'
-
-// useCRUD('insert','POST',{nombre:Javier,apellido:Garcia}); 
